e2a65e3064b6ff15df54768f008c1a770efed2d5
Shan Shi <a455898334@gmail.com>
2013-04-07 16:45:41 -0400

    importing jquery

--- edges.js
+++ edges.js
@@ -1,3 +1,7 @@
+var script = document.createElement('script');
+script.src = '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
 var all = $("*");
 var canv = document.createElement('canvas');
 for (var i=0; i<all.length; i++) {
