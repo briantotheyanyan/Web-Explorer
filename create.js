eff0ebf71372522f6ceb923ab2bca2c02c13d3e0
Daryl Sew <darylsew@gmail.com>
2013-04-28 02:34:29 -0400

    5000%

--- create.js
+++ create.js
@@ -9,7 +9,7 @@
 //script.src = "http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js";
 //script.type = "text/javascript";
 
-document.getElementsByTagName('head')[0].appendChild(script);
+//document.getElementsByTagName('head')[0].appendChild(script);
 
 ////////////////////////////
 // CANVAS //////////////////
